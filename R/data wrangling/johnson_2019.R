2115af0e2d3a69c3b446a151cc6c158c210723b4
AlbanSagouis <25483578+AlbanSagouis@users.noreply.github.com>
2020-06-03 14:21:13 +0200

    yearlising johnson_2019

--- R/data wrangling/johnson_2019.R
+++ R/data wrangling/johnson_2019.R
@@ -1,72 +1,80 @@
 ## johnson_2019
-
+library(data.table)
 
 dataset_id <- 'johnson_2019'
 load(file='data/raw data/johnson_2019/ddata')
-
-dat <- data.frame(dataset_id = rep(dataset_id, nrow(ddata)))
-
-dat$year <- ddata$Year
-dat$month <- match(ddata$Month, month.name)
-dat$day <- NA
-
-dat$site <- ddata$Creek
-dat$block <- ddata$Branch
-dat$plot <- ddata$Transect
-dat$subplot <- ddata$Rep
-
+setDT(ddata)
 
 # Typo correction
-ddata[ddata$Nutrient== 'Referenceient_Nutrients', 'Nutrient'] <- 'Reference'
+ddata[Nutrient == 'Referenceient_Nutrients', Nutrient := 'Reference'][,Nutrient := droplevels(Nutrient)]
 
-dat$treatment <- ifelse(ddata$Nutrient == "Reference" & ddata$Fish == 'Ambient', 'control',
-                        ifelse(ddata$Nutrient == "Reference" & ddata$Fish == 'Low', 'fish manipulation',
-                               ifelse(ddata$Nutrient %in% c('Nutrient',"Fertilized") & ddata$Fish == 'Ambient', 'nutrient addition',
-                                      ifelse(ddata$Nutrient %in% c('Nutrient',"Fertilized") & ddata$Fish == 'Low', 'nutrient+fish', NA)
-                               )
-                        )
-                  )
-dat$treatment_type <- dat$treatment
-
-beforeafter <- ifelse(ddata$Period == 'Before', 'B',
-                      ifelse(ddata$Period == 'After', 'A', NA))
-controlimpact <- ifelse(ddata$Nutrient == 'Reference', 'C',
-                        ifelse(ddata$Nutrient == 'Fertilized', 'I', NA))
-dat$design <- paste0(beforeafter, controlimpact)
-
-sampledate <- paste(ddata$Year, ddata$Month, sep = '-')
-timepoints <- seq_along(unique(sampledate))
-timepoints <- paste0('T',timepoints[match(sampledate, unique(sampledate))])
-dat$timepoint <- timepoints
-
-first_disturbance_date <- as.Date(ifelse(ddata$Period == 'After' & ddata$Fish == 'Low' | (ddata$Period == 'After'  & ddata$Creek == 'Sweeney' & ddata$Nutrient == 'Fertilized'), '2004-05-01',
-       ifelse(ddata$Period == 'After' & ddata$Creek == 'Clubhead' & ddata$Nutrient == 'Fertilized', '2005-05-01', NA)), format='%Y-%m-%d')
-
-dat$time_since_disturbance_days <- ifelse(is.na(first_disturbance_date), NA,
-                                          as.numeric(as.Date(paste(ddata$Year, ddata$Month, '01', sep='-'), format = '%Y-%B-%d') - first_disturbance_date)
-)
-
-
-dat$realm <- 'terrestrial'
-dat$taxon <- 'invertebrates'
-
-
+# Wide to long conversion
 species_columns <- grep(colnames(ddata), pattern = 'num.')
 colnames(ddata) <- trimws( gsub(colnames(ddata), pattern = 'num\\.', replacement = ' ') )
-dat <- data.frame(dat, ddata[, species_columns])
+ddata <- melt(ddata, measure.vars = colnames(ddata)[species_columns], variable.name = 'species', value.name = 'value')
 
-# wide to long conversion
-dat <- reshape2::melt(dat, measure.vars = colnames(ddata)[species_columns], variable.name = 'species', value.name = 'value')
+# Deleting useless rows
+ddata <- ddata[value > 0] # , grep(colnames(ddata), pattern = 'Total', value = TRUE) := NULL
+
+timepoints <- seq_along(unique(ddata$Year))
+timepoints <- paste0('T',timepoints[match(ddata$Year, unique(ddata$Year))])
+
+
+fdd = ifelse(ddata$Period == 'After' & ddata$Fish == 'Low' | (ddata$Period == 'After'  & ddata$Creek == 'Sweeney' & ddata$Nutrient == 'Fertilized'), 2004,
+             ifelse(ddata$Period == 'After' & ddata$Creek == 'Clubhead' & ddata$Nutrient == 'Fertilized', 2005, NA))
+
+
+ddata[, ':='(site = Creek,
+             year = Year,
+             treatment = ifelse(Nutrient == "Reference" & Fish == 'Ambient', 'control',
+                                ifelse(Nutrient == "Reference" & Fish == 'Low', 'fish manipulation',
+                                       ifelse(Nutrient %in% c('Nutrient',"Fertilized") & Fish == 'Ambient', 'nutrient addition',
+                                              ifelse(Nutrient %in% c('Nutrient',"Fertilized") & Fish == 'Low', 'nutrient+fish', NA)
+                                       )
+                                )
+             ),
+
+
+             timepoints = timepoints,
+             fdd = fdd
+
+             )]
+ddata[, ':='(design = paste0(ifelse(Period == 'Before', 'B',
+                                    ifelse(Period == 'After', 'A', NA)),
+                             ifelse(treatment == 'control', 'C', 'I')),
+             time_since_disturbance = year - fdd
+)
+]
+
+
+# Standardisation
+dtsampling <- ddata[order(year, treatment), .(effort = length(unique(Month))), by = .(site, year, treatment)]
+dat <- ddata[, .(value = sum(value)),
+             by = .(site, year, treatment, species)]
+dat <- merge(dat, dtsampling, by = c('year', 'site', 'treatment'))
+dat[, value := value / effort][, effort := NULL]
+
+# Metadata
+metadata <- unique(ddata[, .(site, year, treatment, fdd, timepoints, design, time_since_disturbance)])
+dat <- merge(dat, metadata, by = c('year', 'site', 'treatment'))
+
+dat[, ':='(
+   dataset_id = dataset_id,
+   treatment_type = 'eutrophication and community manipulation',
+   realm = 'terrestrial',
+   taxon = 'invertebrates',
+   metric = 'density',
+   unit = 'ind/m2',
+   comment = 'Block design with two treatments. Treatments were applied at different times and frequencies. Treatments applied to the different sites differed so sites were not pooled together. Abundances from several samplings a year, several transects were pooled together.'
+)]
+
+
 dat <- dat[dat$value > 0, ]
 
-dat$metric <- 'density'
-dat$value <- dat$value * 292.4
-dat$unit <- 'ind/m2'
 
-dat$comment <- 'Block design with two treatments. Treatments were applied at different times and frequencies.'
 
 dat <- dat[!is.na(dat$value), ]
 
 dir.create(paste0('data/wrangled data/', dataset_id), showWarnings = FALSE)
-write.csv(dat, paste0('data/wrangled data/', dataset_id, "/", dataset_id, '.csv'),
+write.csv(dat, paste0('data/wrangled data/', dataset_id, "/", dataset_id, '_dt.csv'),
           row.names=FALSE)
